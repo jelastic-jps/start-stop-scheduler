0e02e49d58bdbf657905723eee97057f94c5e57c
sych74 <sych74@gmail.com>
2017-04-23 13:51:44 +0200

    Create uninstall.js

---
+++ scripts/uninstall.js
@@ -0,0 +1,9 @@
+//@auth @req(name)
+
+jelastic.dev.scripting.DeleteScript(name);
+
+var tasks = jelastic.utils.scheduler.GetTasks().objects;
+for (var i = 0, l = tasks.length; i < l; i++) 
+  if (tasks[i].script == name) jelastic.utils.scheduler.RemoveTask(tasks[i].id);
+
+return {result:0}
