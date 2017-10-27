44120abaf0c161907f7bde0a419302ba31c225e3
Ruslan Synytsky <siruslan@gmail.com>
2017-10-27 16:49:40 +0200

    Update create-service.js

--- scripts/create-service.js
+++ scripts/create-service.js
@@ -44,7 +44,7 @@ function addTask(cron, taskName) {
     });
 
     for (var i = 0, l = quartz.length; i < l; i++) {
-        var resp = jelastic.utils.scheduler.CreateEnvTask({envName: envName, session: session, script: name, trigger: "cron:" + quartz[i]}, description: description, params: params}) 
+        var resp = jelastic.utils.scheduler.CreateEnvTask({envName: envName, session: session, script: name, trigger: "cron:" + quartz[i], description: description, params: params}) 
         if (resp.result != 0) return buildErrorMessage(resp)
     }
     
