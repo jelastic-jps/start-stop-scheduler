1213f877e6758ed25b0a2cf1514f4b46df667f3a
dzotic9 <dzotic9@gmail.com>
2017-11-01 16:10:38 +0200

    Update uninstall.js

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -4,7 +4,7 @@ jelastic.dev.scripting.DeleteScript({session: session, name:name});
 
 var tasks = jelastic.utils.scheduler.GetTasks({session: session}).objects;
 for (var i = 0, l = tasks.length; i < l; i++) 
-  if (tasks[i].script == name) jelastic.utils.scheduler.RemoveTask({session:session, id: tasks[i].id});
+  if (tasks[i].script == name) jelastic.utils.scheduler.RemoveTask({appid: appid, session:session, id: tasks[i].id});
 
 
 return {result:0}
