c6040abfca9963e30c4d0b334202d35f50342cb5
dzotic9 <dzotic9@gmail.com>
2018-12-01 11:04:10 +0200

    Update start-stop.js

--- scripts/start-stop.js
+++ scripts/start-stop.js
@@ -1,16 +1,16 @@
 //@auth @req(action, envName)
 
-var a = action + '', c = jelastic.env.control, e = envName, s = session, r, s;
-s = c.GetEnvInfo(e, session).env.status;
+var a = action + '', c = jelastic.env.control, e = envName, s = session, r, status;
+status = c.GetEnvInfo(e, s).env.status;
 
 switch (a) {
     case 'start': 
-        if (s == 2 || s ==4) {
+        if (status == 2 || status ==4) {
             r = c.StartEnv(e, s); 
         }
         break;
     case 'stop': 
-        if (s == 1) {
+        if (status == 1) {
             r = c.StopEnv(e, s, -1);
         }
         break;
@@ -18,4 +18,6 @@ switch (a) {
     default: r = {result: 99, error: 'unknown action [' + a + ']'}
 }
 
-return r;
+return r || {
+    result: 0
+};
