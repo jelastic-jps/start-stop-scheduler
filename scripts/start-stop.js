23dcdd7a52bba84fedf267b02814c1c714fa94ab
dzotic9 <dzotic9@gmail.com>
2018-12-01 10:51:21 +0200

    Update start-stop.js

--- scripts/start-stop.js
+++ scripts/start-stop.js
@@ -1,9 +1,19 @@
 //@auth @req(action, envName)
 
-var a = action + '', c = jelastic.env.control, e = envName, s = session, r;
+var a = action + '', c = jelastic.env.control, e = envName, s = session, r, s;
+s = c.GetEnvInfo(e, session).env.status;
+
 switch (a) {
-    case 'start': r = c.StartEnv(e, s); break;
-    case 'stop': r = c.StopEnv(e, s, -1); break;
+    case 'start': 
+        if (s == 2 || s ==4) {
+            r = c.StartEnv(e, s); 
+        }
+        break;
+    case 'stop': 
+        if (s == 1) {
+            r = c.StopEnv(e, s, -1);
+        }
+        break;
     case 'sleep': r = c.SleepEnv(e, s); break;
     default: r = {result: 99, error: 'unknown action [' + a + ']'}
 }
