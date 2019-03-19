d873db7db0b7057e24e507dd3b4e44e2c4597df0
alexey.lazarenko <dzotic9@gmail.com>
2019-03-19 17:07:02 +0200

    JE-46285

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -1,10 +1,11 @@
 //@auth @req(name)
 
-jelastic.dev.scripting.DeleteScript({appid: appid, session: session, name:name});
+jelastic.dev.scripting.DeleteScript({appid: appid, session: session, name: name});
+jelastic.dev.scripting.DeleteScript({appid: appid, session: session, name: "uninstall-" + name});
 
 var tasks = jelastic.utils.scheduler.GetTasks({appid: appid, session: session}).objects;
 for (var i = 0, l = tasks.length; i < l; i++) 
-  if (tasks[i].script == name) jelastic.utils.scheduler.RemoveTask({appid: appid, session:session, id: tasks[i].id});
+  if (tasks[i].script.indexOf(name) > -1) jelastic.utils.scheduler.RemoveTask({appid: appid, session:session, id: tasks[i].id});
 
 
 return {result:0}
