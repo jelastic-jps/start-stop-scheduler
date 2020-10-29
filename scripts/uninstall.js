9d340b673105eb6a7ec8016f65b1d2f0c0589233
vlobzakov <39057925+vlobzakov@users.noreply.github.com>
2020-10-29 10:46:07 +0200

    Update uninstall.js

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -1,13 +1,13 @@
 //@auth @req(name)
 
-jelastic.dev.scripting.DeleteScript({appid: "${globals.appid}", session: session, name: name});
-jelastic.dev.scripting.DeleteScript({appid: "${globals.appid}", session: session, name: "uninstall-" + name});
+jelastic.dev.scripting.DeleteScript({appid: appid + "/${globals.appid}", session: session, name: name});
+jelastic.dev.scripting.DeleteScript({appid: appid + "/${globals.appid}", session: session, name: "uninstall-" + name});
 
-var tasks = jelastic.utils.scheduler.GetTasks({appid: "${globals.appid}", session: session}).objects;
+var tasks = jelastic.utils.scheduler.GetTasks({appid: appid + "/${globals.appid}", session: session}).objects;
 var delTasks = [];
 for (var i = 0, l = tasks.length; i < l; i++) {
     if (tasks[i].script == name) delTasks.push(tasks[i].id); 
 }
-if (delTasks.length) jelastic.utils.scheduler.DeleteTasks({appid: "${globals.appid}", session:session, ids: delTasks});
+if (delTasks.length) jelastic.utils.scheduler.DeleteTasks({appid: appid + "/${globals.appid}", session:session, ids: delTasks});
 
 return {result:0}
