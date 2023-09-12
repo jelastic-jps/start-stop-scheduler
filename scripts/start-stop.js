d83f067bfb20d76f9079fae29658e26dea4db6b9
alexey.lazarenko <dzotic9@gmail.com>
2023-09-12 12:25:43 +0300

    Update start-stop.js

--- scripts/start-stop.js
+++ scripts/start-stop.js
@@ -1,33 +1,54 @@
 //@auth @req(action, envName)
 import com.hivext.api.server.system.service.utils.EnvironmentStatus;
 
-var a = action + '', c = jelastic.env.control, e = envName, s = session, r, status, resp,
+var action = action + '', c = jelastic.env.control, e = envName, s = session, result, status, resp,
     DOWN = 'ENV_STATUS_TYPE_DOWN',
     SLEEP = 'ENV_STATUS_TYPE_SLEEP',
-    RUNNING = 'ENV_STATUS_TYPE_RUNNING';
+    RUNNING = 'ENV_STATUS_TYPE_RUNNING',
+    description = "start-stop-scheduler";
 
 resp = c.GetEnvInfo(e, s);
 if (resp.result != 0) return resp;
 
 status = resp.env.status;
 
-switch (a) {
+switch (action) {
     case 'start': 
         if (status == EnvironmentStatus[DOWN].getValue() || 
             status == EnvironmentStatus[SLEEP].getValue()) {
-            r = c.StartEnv(e, s); 
+            result = c.StartEnv(e, s);
+            if (result != 0) return createRetryTask();
         }
         break;
     case 'stop': 
         if (status == EnvironmentStatus[RUNNING].getValue()) {
-            r = c.StopEnv(e, s, -1);
+            result = c.StopEnv(e, s, -1);
+            if (result != 0) return createRetryTask();
         }
         break;
-    case 'sleep': r = c.SleepEnv(e, s); break;
-    default: r = {result: 99, error: 'unknown action [' + a + ']'}
+    case 'sleep': result = c.SleepEnv(e, s); break;
+    default: result = {result: 99, error: 'unknown action [' + action + ']'}
 }
 
-return r || {
+function createRetryTask() {
+    var params = toJSON({
+        action: action,
+        envName: envName,
+        envAppid: envAppid
+    })
+    var targetAppid = api.dev.apps.CreatePersistence ? envAppid : appid;
+    return jelastic.utils.scheduler.CreateEnvTask({
+        appid: targetAppid,
+        envName: envName,
+        session: session,
+        script: name + "-start-stop",
+        trigger: "once_delay:10000",
+        description: description,
+        params: params
+    });
+}
+
+return result || {
     result: 0,
-    message: "Unable to " + a + " environment, status is "+ status 
+    message: "Unable to " + action + " environment, status is "+ status
 };
